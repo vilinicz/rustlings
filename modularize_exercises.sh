a45acc7ecfc79d06bfa2e969e726a692351df489
vilinicz <v.ilinicz@gmail.com>
2024-03-16 22:39:45 +0400

    modularize exercises: include quiz files

--- modularize_exercises.sh
+++ modularize_exercises.sh
@@ -2,20 +2,19 @@
 
 # Empty the file
 > exercises/exercises.rs
-echo "// this file was auto-generated with modularize_exercises.sh script" >> exercises/exercises.rs
 
-# Loop over all .rs files in the subdirectories of the exercises directory
-for dir in ./exercises/*; do
-    if [ -d "$dir" ]; then
-        for file in $(find $dir -type f -name "*.rs"); do
-            # Remove the './exercises/' prefix from the file path
-            path=${file#./exercises/}
+echo '// This file was auto-generated with ../modularize_exercises.sh script' >> exercises/exercises.rs
+# Loop over all .rs files in the exercises directory
+for file in $(find ./exercises -type f -name "*.rs"); do
+    # Remove the 'exercises/' prefix from the file path
+    path=${file#./exercises/}
 
-            # Write the path and module name to the file
-            echo "#[path=\"$path\"]" >> exercises/exercises.rs
-            echo "pub mod $(basename $file .rs);" >> exercises/exercises.rs
-        done
+    # Write the path and module name to the file only if $path variable is present and contains a '/'
+    if [[ -n "$path" && "$path" == */* ]]; then
+        echo "#[path=\"$path\"]" >> exercises/exercises.rs
     fi
+
+    echo "pub mod $(basename $file .rs);" >> exercises/exercises.rs
 done
 
 echo 'Done'
